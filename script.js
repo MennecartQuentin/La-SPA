c58afd6669a4075fa82a8bbf40c4b0137c3eb378
MennecartQuentin <mennecartquentin16@gmail.com>
2022-02-18 17:24:19 +0100

    modification responsive0.1

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ $(document).ready(function() {
         });
     });
 });
+
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += "responsive";
+    } else {
+        x.className = "nav";
+    }
+}
