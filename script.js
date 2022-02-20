47d8f5b4079ba3deedc417dd01c0585a074b69cb
MennecartQuentin <mennecartquentin16@gmail.com>
2022-02-20 15:27:10 +0100

    PatchEval-1 Final

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-$(document).ready(function() {
-
-    $(".navbar").hide();
-
-    $(function() {
-        $(window).scroll(function() {
-
-            if ($(this).scrollTop() < 150) {
-                $('.navbar').fadeIn();
-            } else {
-                $('.navbar').fadeOut();
-            }
-        });
-    });
-});
-
 function myFunction() {
     var x = document.getElementById("navbar");
     if (x.className === "nav") {
